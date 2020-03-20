d4dae95349592f0b4e98a6b712b23af43f1cb126
Dan Arpoika <darpoika@nmu.edu>
2020-03-20 15:09:28 -0400

    add space in disclaimer

--- components/Footer.js
+++ components/Footer.js
@@ -13,8 +13,7 @@ const Footer = () => (
   <StyledFooter>
     <Container>
       <Disclaimer>
-        <Highlighted>DISCLAIMER:</Highlighted>
-        This model is based off the SIR model with the idea stemming from <a href="http://www.pandemsim.com/data/index.php/make-your-own-sir-model/">this article</a>. This is a simulation only to show the possible effects of government mandated social polices and how they might "flatten the curve". This is a model only and not based off any real data.
+        <Highlighted>DISCLAIMER:</Highlighted> This model is based off the SIR model with the idea stemming from <a href="http://www.pandemsim.com/data/index.php/make-your-own-sir-model/">this article</a>. This is a simulation only to show the possible effects of government mandated social polices and how they might "flatten the curve". This is a model only and not based off any real data.
       </Disclaimer>
     </Container>
   </StyledFooter>
